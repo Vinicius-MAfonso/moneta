235582ca9fb241b4c31157c82021377f624aef9c
Vinicius Afonso <vinicius.mariano.afonso@gmail.com>
2026-08-31 18:18:03 -0300

    feat: add missing class Meta definition in PushSubscription model

--- users/models.py
+++ users/models.py
@@ -1,12 +1,15 @@
 from django.conf import settings
+from django.db import models
 
 
 class PushSubscription(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='push_subscriptions', on_delete=models.CASCADE)
     endpoint = models.URLField(max_length=500)
+    p256dh = models.CharField(max_length=255)
     auth = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
         verbose_name = 'inscrição push'
         verbose_name_plural = 'inscrições push'
         constraints = [
