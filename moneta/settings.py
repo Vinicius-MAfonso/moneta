5ae142510337dfd35371657e90566c62e0d76e6f
Vinicius Afonso <vinicius.mariano.afonso@gmail.com>
2026-09-01 15:37:33 -0300

    feat: enhance CSP policy to allow styles from Google Fonts and add service worker registration check

--- moneta/settings.py
+++ moneta/settings.py
@@ -125,6 +125,7 @@ X_FRAME_OPTIONS = 'DENY'
 DEFAULT_CSP_POLICY = (
     "default-src 'self'; "
     "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com; "
+    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
     "font-src 'self' https://fonts.gstatic.com; "
     "img-src 'self' data: blob:; "
     "connect-src 'self'; "
